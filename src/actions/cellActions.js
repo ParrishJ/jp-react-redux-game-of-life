de29ba60e00ce6d87737c99bfa8be3836baff7c6
jarparrish <jarparrish@gmail.com>
2020-10-16 19:26:26 -0700

    cells are now disabled while game is running

--- src/actions/cellActions.js
+++ src/actions/cellActions.js
@@ -2,6 +2,7 @@ export const TOGGLE_ALIVE = "TOGGLE_ALIVE";
 export const ADVANCE_GENERATION = "ADVANCE_GENERATION";
 export const CLEAR_CELLS = "CLEAR_CELLS";
 export const RANDOMIZE_CELLS = "RANDOMIZE_CELLS";
+export const DISABLE_CELLS = "DISABLE_CELLS";
 
 
 /* export const toggleAlive = (cellId) => (dispatch) => {
@@ -23,4 +24,8 @@ export const clearCells = () => (dispatch) => {
 
 export const randomizeCells = () => (dispatch) => {
     dispatch({ type: RANDOMIZE_CELLS })
+}
+
+export const toggleDisableCells = (bool) => (dispatch) => {
+    dispatch({ type: DISABLE_CELLS, payload: bool })
 }

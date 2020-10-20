69c82750844637e3b86f6cbfe41f4a4ef23cbf33
jarparrish <jarparrish@gmail.com>
2020-10-19 20:04:39 -0700

    about game of life modal implemented

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -5,6 +5,7 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
+import ListGroup from 'react-bootstrap/ListGroup'
 
 import './controlPanel.css'
 
@@ -97,15 +98,34 @@ const ControlPanel = ({advanceGeneration, toggleDisableCells, clearCells, genera
                 </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <p>
-                        Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
-                        commodi aspernatur enim, consectetur. Cumque deleniti temporibus
-                        ipsam atque a dolores quisquam quisquam adipisci possimus
-                        laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
-                        accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
-                        reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
-                        deleniti rem!
-                    </p>
+                    <p>Conway’s Game of Life is a classic computer science simulation that employs 
+                        a few simple rules to create cellular automata that “live”, “die”, and create 
+                        some stunning patterns throughout their collective lifecycles.</p>
+                    <p>The rules for the game of life are simple: A player is presented with a grid of 
+                        cells that can have the condition of being either “dead” or “alive”. At the beginning 
+                        of the game, the player switches the state of any of the cells of their choosing to the 
+                        “alive” state. The player then initializes the simulation.</p>
+                    <p>Once the simulation is initialized, the cells then move on to successive generations where 
+                        they either retain their state of being either “alive” or “dead” or alternate their state 
+                        based on a set of a few simple rules which take into account the state of the cell’s 
+                        “neighbors” - the cells that are directly adjacent to the cell in question. 
+                        The rules are as follows:</p>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>If a cell is alive and has either one or zero neighbors, the cell will be dead during the next generation.</ListGroup.Item>
+                        <ListGroup.Item>If the cell is alive and has either two or three neighbors, the cell will continue to be alive during the next generation.</ListGroup.Item>
+                        <ListGroup.Item>If a cell is alive and has more than three neighbors, it will be dead during the next generation.</ListGroup.Item>
+                        <ListGroup.Item>If a cell is dead and has exactly three neighbors that are alive, it will be alive during the next generation.</ListGroup.Item>
+                        <ListGroup.Item>Lastly, if a cell is dead and has any number of neighbors other than three, the cell will continue to be dead during the next generation.</ListGroup.Item>
+                    </ListGroup>
+                    <p className="mt-3">This simple set of rules allows the cells to either multiply or be reduced from generation to 
+                        generation, sometimes forming some astounding patterns along the way. Perhaps the most interesting 
+                        feature of Conway’s game of life is that it demonstrates that patterns of immense complexity can 
+                        spring forth from a few simple constraints.</p>
+                        <blockquote class="blockquote text-center">
+                            <p class="mb-0 mt-4">Background information on the Conway's Game of Life Sourced By:</p>
+                            <footer class="blockquote-footer">cs.standord.edu, <a href="https://cs.stanford.edu/people/eroberts/courses/soco/projects/2001-02/cellular-automata/beginning/howtoplay.html"><cite title="Source Title">The Game of Life</cite></a></footer>
+                        </blockquote>
+                    
                 </Modal.Body>
             </Modal>
             <Modal

c5f8ed54ca8762ad70da6e50ab82785e7a679971
jarparrish <jarparrish@gmail.com>
2020-11-23 01:26:10 -0700

    fixed favicon

--- src/reducers/cellReducer.test.js
+++ src/reducers/cellReducer.test.js
@@ -1,24 +1,41 @@
 import { cellGenerator } from '../components/generator'
-import { cellReducer } from './cellReducer'
-import * as actions from '../actions/cellActions'
+import { cellReducer, initialState } from './cellReducer'
+import {TOGGLE_STOP_BUTTON} from '../actions/cellActions'
 
 
 describe('cell reducer', () => {
+
+    let state = {
+        generations: 0,
+        cells: cellGenerator(),
+        cellsCopy: cellGenerator(),
+        cellsEnabled: true,
+        gridColors: "Plain",
+        generationActivity: false,
+        buttonsWhileRunning: false,
+        stopButton: true,
+        showGolModal: false,
+        showMeModal: false,
+        showWarningModal: true,
+        }
+
+
+
     it('should return initial state', () => {
-        expect(cellReducer(undefined, {})).toEqual(
-            {
-            generations: 0,
-            cells: cellGenerator(),
-            cellsCopy: cellGenerator(),
-            cellsEnabled: true,
-            gridColors: "Plain",
-            generationActivity: false,
-            buttonsWhileRunning: false,
-            stopButton: true,
-            showGolModal: false,
-            showMeModal: false,
-            showWarningModal: true,
-            }
+        expect(cellReducer(undefined, {})).toEqual(state)
+    })
+})
+
+
+
+
+describe('adds two', () => {
+    function addTwo(num){
+        return(
+            num+num
         )
+    }
+    it('add two numbers', () => {
+        expect(addTwo(2)).toEqual(4)
     })
 })
